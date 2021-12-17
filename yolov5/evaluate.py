dd1afe45ec7d81c356471ed0a04b436ea228b3e3
ManfredGonzalez <gonzalezhernandez.manfred@gmail.com>
2021-12-17 23:37:43 +0100

    evaluate_1ch

--- yolov5/evaluate.py
+++ yolov5/evaluate.py
@@ -140,7 +140,10 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
     # Run inference
     if pt and device.type != 'cpu':
-        model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.parameters())))  # run once
+        if bands_to_apply:
+            model(torch.zeros(1, len(bands_to_apply), *imgsz).to(device).type_as(next(model.parameters())))  # run once
+        else:
+            model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.parameters())))  # run once
     dt, seen = [0.0, 0.0, 0.0], 0
 
     ground_truth_list = []
